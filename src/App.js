bdede18f5a1466a245fce93c66b07cc6e5597b77
Volmon117 <volmon117@gmail.com>
2022-04-07 11:22:33 -0500

    Added the react proyect to github to start saving the progress and deleted unuseful code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" data-section="Home" >
+      <header className="App-header">
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
