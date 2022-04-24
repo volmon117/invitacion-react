84f339e0ce55172d54a34d01d39fdb81e942717a
Volmon117 <volmon117@gmail.com>
2022-04-24 18:07:26 -0500

    Added changes on the peoople component

--- src/Components/People.js
+++ src/Components/People.js
@@ -3,32 +3,61 @@ import { Typography } from "@material-ui/core";
 const People = () => {
   return (
     <p>
-    
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
       <Typography variant="h3" fontFamily="Tangerine">
         Con la bendicion de dios y de nuestros padres
       </Typography>
+      <br/>
       {/* <Icon>
 
       </Icon> */}
-      <Typography variant="h3" fontFamily="Tangerine">
-        Juana Yañez Olea<p>
+      <br/>
+      <Typography variant="h3" fontFamily="Roboto">
+        Juana Yañez Olea
+        <br/>
+        &
+        <br/>
         Alfredo Ponce Olivo
-        </p>
-        <p></p><p>
-            hola
-        </p>
-        <p>
-            hola
-        </p>
-        <p>
-            hola
-        </p>
-        <p>
-            hola
-        </p>
-        <p>
-            hola
-        </p>
+      </Typography>
+      <br/>
+      {/* <Icon>
+
+      </Icon> */}
+      <br/>
+      <Typography variant="h3" fontFamily="Tangerine">
+        Maria de Jesus Hernandez Mendez
+        <br/>
+        &
+        <br/>
+        Santiago Del Blanco Chavez
+      </Typography>
+      <Typography variant="h3" fontFamily="Tangerine">
+        <br/>
+        En Compania de nuestros padrinos
+        <br/>
+        <br/>
+        Patricia Adela Hernandez Mendez
+        <br/>
+        &
+        <br/>
+        J. Jesus Pompa Reyna
       </Typography>
     </p>
   );
--- src/Components/SaveTheDate.js
+++ src/Components/SaveTheDate.js
@@ -4,15 +4,42 @@ const SaveTheDate = () => {
 
     return (
       <>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
         <Typography variant="h3" fontFamily="Tangerine">
-          <p>Save The Date</p>
+          Save The Date
         </Typography>
         <Typography variant="h1" fontFamily="Tangerine">
           Brisia & Santiago
         </Typography>
         <Typography variant="h3" fontFamily="Tangerine">
-          <p>30-09-2022</p>
+          30-09-2022
         </Typography>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
         <Typography variant="h3" fontFamily="Tangerine">
           <p>Las mejores cosas de la vida merecen ser compartidas, gracias por ser parte de nuestra historia...</p>
         </Typography>
